00dfeb2573e3fbdc5e91306b911d65c44be10197
David Ortiz <davidovm94@gmail.com>
2018-11-06 15:14:47 +0100

    add tree

--- src/main.js
+++ src/main.js
@@ -1,12 +1,45 @@
+class Node {
+  constructor (parentNode, value = null, depth, childNodes = {}, isEndOfWord = false) {
+    this.parentNode = parentNode
+    this.childNodes = childNodes
+    this.isEndOfWord = isEndOfWord
+    this.value = value
+    this.depth = depth
+  }
+  addChild(value) {
+    let childNode = this.childNodes[value]
+    if (!childNode) {
+      childNode = new Node(this, value, this.depth + 1)
+      this.childNodes[value] = childNode
+    }
+    return childNode
+  }
+}
 
-const words = ['rat', 'catratdog', 'cat', 'dog', 'cats', 'catsa', 'ratdog', 'e']
+class Tree {
+  constructor () {
+    this.rootNode = new Node(null, 'root', 0)
+  }
+
+  addWord(word) {
+    let currentNode = this.rootNode
+    for (let i = 0; i < word.length; i++) {
+      currentNode = currentNode.addChild(word[i])
+    }
+    currentNode.isEndOfWord = true
+  }
+}
+
+const words = ['rat', 'catratdog', 'cat', 'dog', 'cats', 'catsrat', 'ratdog', 'e']
 const wordLengthsMap = new Map()
+const tree = new Tree()
 let orderedLengths
 
 words.forEach(word => {
   let wordArray = wordLengthsMap.get(word.length) || []
   wordArray.push(word)
   wordLengthsMap.set(word.length, wordArray)
+  tree.addWord(word)
 })
 
 orderedLengths = [...wordLengthsMap.keys()].sort()
