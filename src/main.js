0208e0ddfb106230da2a70349d40431b1a50a9ba
David Ortiz <davidovm94@gmail.com>
2018-11-09 17:49:36 +0100

    tests in main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 const fs = require('fs')
 const treeAlgorithm = require('./algorithms/tree')
+const recursiveAlgorithm = require('./algorithms/recursive')
 
 let file = null, words = null
 try {
   file = fs.readFileSync('src/words.txt', 'utf8')
-  words = file.split(/\r?\n/)
+  words = file.split(/\r?\n/).filter(Boolean).slice(0, 4000)
 } catch (e) {
   console.log('please provide a valid file', e)
 }
 
+let solution
+
+console.log('Tree algorithm execution:')
 console.time("dbsave")
-const {concatenatedWordCount, maxLengthWords, secondMaxLengthWords } = treeAlgorithm.computeConcatenatedWords(words)
+solution = treeAlgorithm.computeConcatenatedWords(words)
 console.timeEnd("dbsave")
-console.log(maxLengthWords, secondMaxLengthWords, concatenatedWordCount)
+console.log(solution.maxLengthConcatWords, solution.secondMaxLengthConcatWords, solution.concatenatedWordCount)
+
+// console.log('Recursive algorithm execution:')
+// console.time("dbsave")
+// solution = recursiveAlgorithm.computeConcatenatedWords(words)
+// console.timeEnd("dbsave")
+// console.log(solution.maxLengthConcatWords, solution.secondMaxLengthConcatWords, solution.concatenatedWordCount)
